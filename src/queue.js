cd7fffeb6cf9002093ab663b77a6cba20a768b2c
Joe Gordon <joegordon@PEG-MAC-009914.local>
2018-04-18 11:19:53 +0100

    asyncify

--- src/queue.js
+++ src/queue.js
@@ -2,32 +2,25 @@ const open = require('amqplib').connect('amqp://rabbit');
 
 const createChannel = open.then(conn  => conn.createChannel())
 
-const createSendToQueue = queueName => message => createChannel
-.then(ch => {
-  ch.assertQueue(queueName)
-  .then(() => ch.sendToQueue(queueName, new Buffer(JSON.stringify(message))))
-})
+const queue = async queueName => {
+  const ch = await createChannel
+  await ch.assertQueue(queueName)
+  return ch
+}
 
-// const createConsumeQueue = queueName => callback => {
-//   const channel = await createChannel
-//   await ch.assertQueue(queueName)
-//   ch.consume(queueName, msg => {
-//     callback(msg.content.toString(), () => {
-//       ch.ack(msg)
-//     })
-//   })
-// }
-const createConsumeQueue = queueName => callback => createChannel
-.then(ch => {
-  ch.assertQueue(queueName)
-  .then(() => {
-    ch.consume(queueName, msg => {
-      callback(msg.content.toString(), () => {
-        ch.ack(msg)
-      })
+const createSendToQueue = queueName => async message => {
+  const ch = await queue(queueName)
+  ch.sendToQueue(queueName, new Buffer(JSON.stringify(message)))
+}
+
+const createConsumeQueue = queueName => async callback => {
+  const ch = await queue(queueName)
+  ch.consume(queueName, msg => {
+    callback(msg.content.toString(), () => {
+      ch.ack(msg)
     })
   })
-})
+}
 
 const consumeSubmitter = createConsumeQueue('SUBMITTER')
 const consumeFabricator = createConsumeQueue('SUBMITTER')
